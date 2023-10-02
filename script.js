e286f7f4eb454ea03d944d09d301a1c7440517a9
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-02 20:05:34 +0530

    sound Added

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let bulb = document.getElementById("bulb");
 let btn = document.getElementsByTagName("button");
 let flag = 0;
 let buttonBackground = btn[0].parentElement;
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 btn[0].addEventListener('click', function(){
     if(flag == 0){
+        audio.play();
         bulb.style.backgroundColor="yellow";
         bulb.style.boxShadow = "0 0 120px 450px rgba(255, 255, 0, 0.5)"
         bulb.style.transition = "0.2s"
@@ -16,17 +18,18 @@ btn[0].addEventListener('click', function(){
         btn[0].style.transition = "0.15s"
         flag=1;
         }
-        else{
-            bulb.style.transition = "0.2s"
-            bulb.style.backgroundColor="rgba(255, 255, 0 ,0.2)";
-            bulb.style.boxShadow = "none";
-            buttonBackground.style.backgroundColor = "rgb(46, 46, 46)";
-            buttonBackground.style.boxShadow = "none"
-            buttonBackground.style.transition = "0.15s"
-            btn[0].style.border = '1px solid black';
-            btn[0].style.borderBottom = '8px solid rgb(121, 120, 120)';
-            btn[0].style.backgroundImage = "linear-gradient(rgb(120, 120, 120), rgb(90, 90, 90))";
-            btn[0].style.transition = "0.15s"
-            flag=0;
-        }
+    else{
+        audio.play();
+        bulb.style.transition = "0.2s"
+        bulb.style.backgroundColor="rgba(255, 255, 0 ,0.2)";
+        bulb.style.boxShadow = "none";
+        buttonBackground.style.backgroundColor = "rgb(46, 46, 46)";
+        buttonBackground.style.boxShadow = "none"
+        buttonBackground.style.transition = "0.15s"
+        btn[0].style.border = '1px solid black';
+        btn[0].style.borderBottom = '8px solid rgb(121, 120, 120)';
+        btn[0].style.backgroundImage = "linear-gradient(rgb(120, 120, 120), rgb(90, 90, 90))";
+        btn[0].style.transition = "0.15s"
+        flag=0;
+    }
 });
