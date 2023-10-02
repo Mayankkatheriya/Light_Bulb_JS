c26fa3a6bee66671c44b5b1d14a91edea9a126b1
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-02 15:44:55 +0530

    button changes

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ btn[0].addEventListener('click', function(){
     if(flag == 0){
         bulb.style.backgroundColor="yellow";
         bulb.style.boxShadow = "0 0 120px 450px rgba(255, 255, 0, 0.5)"
-        bulb.style.transition = "0.15s"
-        btn[0].style.backgroundColor = "rgba(0, 120, 0 , 0.8)"
-        btn[0].style.color = "#a696df"
+        bulb.style.transition = "0.2s"
+        btn[0].style.border = '1px solid black';
+        btn[0].style.borderTop = '8px solid rgb(121, 120, 120)';
+        btn[0].style.backgroundImage = "linear-gradient(rgb(90, 90, 90), rgb(116, 115, 115))";
+        btn[0].style.transition = "0.15s"
         flag=1;
         }
         else{
+            bulb.style.transition = "0.2s"
             bulb.style.backgroundColor="rgba(255, 255, 0 ,0.2)";
-            bulb.style.boxShadow = "none"
-            btn[0].style.backgroundColor = "rgba(255, 0 ,0 ,0.4)"
+            bulb.style.boxShadow = "none";
+            btn[0].style.border = '1px solid black';
+            btn[0].style.borderBottom = '8px solid rgb(121, 120, 120)';
+            btn[0].style.backgroundImage = "linear-gradient(rgb(116, 115, 115), rgb(90, 90, 90))";
+            btn[0].style.transition = "0.15s"
             flag=0;
         }
 });
