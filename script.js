de9a34c165b2f245c6826946ff3514fa3779c06e
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-02 16:05:53 +0530

    final update

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let bulb = document.getElementById("bulb");
 let btn = document.getElementsByTagName("button");
 let flag = 0;
+let buttonBackground = btn[0].parentElement;
 btn[0].addEventListener('click', function(){
     if(flag == 0){
         bulb.style.backgroundColor="yellow";
         bulb.style.boxShadow = "0 0 120px 450px rgba(255, 255, 0, 0.5)"
         bulb.style.transition = "0.2s"
+        buttonBackground.style.backgroundColor = "rgb(120, 122, 112)";
+        buttonBackground.style.boxShadow = "0 0 10px 3px #000"
+        buttonBackground.style.transition = "0.15s"
         btn[0].style.border = '1px solid black';
         btn[0].style.borderTop = '8px solid rgb(121, 120, 120)';
-        btn[0].style.backgroundImage = "linear-gradient(rgb(90, 90, 90), rgb(116, 115, 115))";
+        btn[0].style.backgroundImage = "linear-gradient(rgb(110,110,110), rgb(120,120,120))";
         btn[0].style.transition = "0.15s"
         flag=1;
         }
@@ -16,9 +20,12 @@ btn[0].addEventListener('click', function(){
             bulb.style.transition = "0.2s"
             bulb.style.backgroundColor="rgba(255, 255, 0 ,0.2)";
             bulb.style.boxShadow = "none";
+            buttonBackground.style.backgroundColor = "rgb(46, 46, 46)";
+            buttonBackground.style.boxShadow = "none"
+            buttonBackground.style.transition = "0.15s"
             btn[0].style.border = '1px solid black';
             btn[0].style.borderBottom = '8px solid rgb(121, 120, 120)';
-            btn[0].style.backgroundImage = "linear-gradient(rgb(116, 115, 115), rgb(90, 90, 90))";
+            btn[0].style.backgroundImage = "linear-gradient(rgb(120, 120, 120), rgb(90, 90, 90))";
             btn[0].style.transition = "0.15s"
             flag=0;
         }
